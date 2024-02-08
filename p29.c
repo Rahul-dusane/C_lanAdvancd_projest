2e176605d5fe88a668bf4bdc2a81d8fdb203ce49
Rahul-dusane <152960290+Rahul-dusane@users.noreply.github.com>
2024-02-08 18:52:57 +0530

    Update p29.c

--- p29.c
+++ p29.c
@@ -4,6 +4,10 @@ int main(){
     int m;
     printf("Enter the number.\n");
     scanf("%d",&m);
+    if(m<=0){
+        printf("Enter positiv integer.\n");
+        return 0;
+    }
     int sum=add(m);
     printf("sum = %d",sum);
 }
